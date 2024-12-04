8e33288eaea50aaec58103f467d07ccccfe4c80b
Jsinger03 <julian.singer@gmail.com>
2024-12-04 15:00:35 -0500

    created User abstract class (might change to interface, depends on UML)

---
+++ src/finalProject/User.java
@@ -0,0 +1,10 @@
+package finalProject;
+
+public abstract class User {
+	private String username;
+	private String password;
+	public User(String usern, String pwd) {
+		username = usern;
+		password = pwd;
+	}
+}
