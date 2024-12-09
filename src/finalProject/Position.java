9597a74d7e42fc48f893067ed9a2e648a5da6bbc
rafedabbassi <109957195+rafedabbassi@users.noreply.github.com>
2024-12-08 19:56:00 -0500

    Update Position.java
    getters + setters

--- src/finalProject/Position.java
+++ src/finalProject/Position.java
@@ -1,3 +1,4 @@
+package finalProject;
 
 public class Position {
 	Stock stock;
@@ -8,5 +9,47 @@ public class Position {
 	float total_cost = purchase_price * num_shares;
 	float total_value = stock.getPrice() * num_shares;
 	float profit_loss = total_value - total_cost;
+	public Stock getStock() {
+		return stock;
+	}
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+	public float getPurchase_price() {
+		return purchase_price;
+	}
+	public void setPurchase_price(float purchase_price) {
+		this.purchase_price = purchase_price;
+	}
+	public float getNum_shares() {
+		return num_shares;
+	}
+	public void setNum_shares(float num_shares) {
+		this.num_shares = num_shares;
+	}
+	public float getCurrent_value() {
+		return current_value;
+	}
+	public void setCurrent_value(float current_value) {
+		this.current_value = current_value;
+	}
+	public float getTotal_cost() {
+		return total_cost;
+	}
+	public void setTotal_cost(float total_cost) {
+		this.total_cost = total_cost;
+	}
+	public float getTotal_value() {
+		return total_value;
+	}
+	public void setTotal_value(float total_value) {
+		this.total_value = total_value;
+	}
+	public float getProfit_loss() {
+		return profit_loss;
+	}
+	public void setProfit_loss(float profit_loss) {
+		this.profit_loss = profit_loss;
+	}
 	
 }
