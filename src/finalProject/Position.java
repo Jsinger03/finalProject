5cf9a8ba1cff65962feec0c08efce5eda9c597d0
rafedabbassi <109957195+rafedabbassi@users.noreply.github.com>
2024-12-08 19:42:09 -0500

    Update Position.java
    p&l

--- src/finalProject/Position.java
+++ src/finalProject/Position.java
@@ -1,6 +1,12 @@
 
 public class Position {
 	Stock stock;
+	float purchase_price;
 	float num_shares;
 	float current_value;
+	
+	float total_cost = purchase_price * num_shares;
+	float total_value = stock.getPrice() * num_shares;
+	float profit_loss = total_value - total_cost;
+	
 }
