531eab0ab51ca2cf72c22994d9789e8891ce3134
rafedabbassi <109957195+rafedabbassi@users.noreply.github.com>
2024-12-08 22:30:04 -0500

    Update Admin.java

--- src/finalProject/Admin.java
+++ src/finalProject/Admin.java
@@ -1,3 +1,47 @@
-public class Admin extends User {
-  // add method for add/remove stock
+package finalProject;
+import java.util.*;
+
+public class Admin extends User{
+	
+	Scanner sc = new Scanner(System.in);
+
+	// super constructor
+	public Admin(String usern, String pwd) {
+		super(usern, pwd);
+	}
+	
+	// global universe of stocks
+	public static ArrayList<Stock> stocks;
+
+	// add stock to universe
+	public void add_stock() {
+		System.out.println("Enter stock name: ");
+		String name = sc.nextLine();
+		System.out.println("Enter stock symbol: ");
+		String symbol = sc.nextLine();
+		System.out.println("Enter stock price: ");
+		float price = sc.nextFloat();
+		Stock new_stock = new Stock(name, symbol, price);
+		stocks.add(new_stock);
+	}
+	
+	// update stock price
+	public void update_price(String name, float price) {
+		Boolean found = false;
+		for(int i = 0; i < stocks.size(); i++) {
+			if(stocks.get(i).getName() == name) {
+				stocks.get(i).setPrice(price);
+				found = true;
+			}
+		}
+		if(found == false) {
+			System.out.println("Stock not found in universe.");
+		}
+	}
+	
+	// generate report for user
+	public void generate_report(User user) {
+		// search through users, then if found, print report of all positions, p&l, etc.
+	}
+	
 }
