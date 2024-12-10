4fb68c546f25986aa7c8cc1d0b1ff4af962c8bbe
Jsinger03 <julian.singer@gmail.com>
2024-12-10 11:27:41 -0500

    updated dbmanager for logins / users

--- src/finalProject/UserDBManager.java
+++ src/finalProject/UserDBManager.java
@@ -2,6 +2,27 @@ package finalProject;
 import java.io.File;
 
 public class UserDBManager {
-	private File userDB;
+	private File customerDB;
+	private File adminDB;
 
+	public UserDBManager() {
+		customerDB = new File("db/customerDB.csv");
+		adminDB = new File("db/adminDB.csv");
+	}
+    public Customer loginCustomer(String username, String password) {
+        //search customerDB for matching username
+        //if found, check if password matches
+        //if matches, return customer object
+        //if not found or password does not match, return null
+        return null;
+    }
+
+    public Admin loginAdmin(String username, String password) {
+        //search adminDB for matching username
+        //if found, check if password matches
+        //if matches, return admin object
+        //if not found or password does not match, return null
+        return null;
+    }
 }
+
