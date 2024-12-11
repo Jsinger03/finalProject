9a53f54653505fcc5e85f17c584003991fff8433
Jsinger03 <julian.singer@gmail.com>
2024-12-10 21:37:22 -0500

    Transaction has constructor and to and from string methods

--- src/finalProject/Transaction.java
+++ src/finalProject/Transaction.java
@@ -3,80 +3,24 @@ package finalProject;
 import java.time.LocalDateTime;
 
 public class Transaction {
-    public enum TransactionType { BUY, SELL }
-
-    private String id;
-    private String userId;
-    private String stockSymbol;
+    private Stock stock;
     private int quantity;
     private double price;
     private LocalDateTime date;
-    private TransactionType type;
-
-    public Transaction(String id, String userId, String stockSymbol, int quantity, double price, LocalDateTime date, TransactionType type) {
-        this.id = id;
-        this.userId = userId;
-        this.stockSymbol = stockSymbol;
+    private String type;
+    public Transaction(Stock stock, int quantity, double price, LocalDateTime date, String type) {
+        this.stock = stock;
         this.quantity = quantity;
         this.price = price;
         this.date = date;
         this.type = type;
     }
-
-    // Getters and Setters
-    public String getId() {
-        return id;
+    public String toString() {
+        //transaction is stored as stock,quantity,price,date,type
+        return stock.toString() + ":" + quantity + ":" + price + ":" + date.toString() + ":" + type;
     }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getStockSymbol() {
-        return stockSymbol;
-    }
-
-    public void setStockSymbol(String stockSymbol) {
-        this.stockSymbol = stockSymbol;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public TransactionType getType() {
-        return type;
-    }
-
-    public void setType(TransactionType type) {
-        this.type = type;
+    public static Transaction fromString(String str) {
+        String[] parts = str.split(":");
+        return new Transaction(Stock.fromString(parts[0]), Integer.parseInt(parts[1]), Double.parseDouble(parts[2]), LocalDateTime.parse(parts[3]), parts[4]);
     }
 }
