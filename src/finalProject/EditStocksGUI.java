bd5fd5d83cf04b18b81cf539a4e1854cf6872af9
Jsinger03 <julian.singer@gmail.com>
2024-12-11 11:57:07 -0500

    updated admin ediut gui

--- src/finalProject/EditStocksGUI.java
+++ src/finalProject/EditStocksGUI.java
@@ -1,18 +1,16 @@
 package finalProject;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JList;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class EditStocksGUI extends JFrame {
 
@@ -44,37 +42,37 @@ public class EditStocksGUI extends JFrame {
         JList listStocks = new JList(displayableStocks);
 
         JScrollPane scrollPane = new JScrollPane(listStocks);
-        scrollPane.setBounds(24, 16, 399, 137);
+        scrollPane.setBounds(6, 6, 399, 137);
         contentPane.add(scrollPane);
 		
 		txtStockName = new JTextField();
-		txtStockName.setBounds(28, 193, 130, 26);
+		txtStockName.setBounds(6, 193, 130, 26);
 		contentPane.add(txtStockName);
 		txtStockName.setColumns(10);
 		
 		txtStockSymbol = new JTextField();
-		txtStockSymbol.setBounds(170, 193, 130, 26);
+		txtStockSymbol.setBounds(148, 193, 130, 26);
 		contentPane.add(txtStockSymbol);
 		txtStockSymbol.setColumns(10);
 		
 		txtStockPrice = new JTextField();
-		txtStockPrice.setBounds(314, 193, 130, 26);
+		txtStockPrice.setBounds(285, 193, 130, 26);
 		contentPane.add(txtStockPrice);
 		txtStockPrice.setColumns(10);
 		
 		JLabel lblName = new JLabel("Name");
-		lblName.setBounds(66, 165, 61, 16);
+		lblName.setBounds(16, 165, 61, 16);
 		contentPane.add(lblName);
 		
 		JLabel lblSymbol = new JLabel("Symbol");
-		lblSymbol.setBounds(196, 165, 61, 16);
+		lblSymbol.setBounds(148, 165, 61, 16);
 		contentPane.add(lblSymbol);
 		
 		JLabel lblPrice = new JLabel("Price");
-		lblPrice.setBounds(340, 165, 61, 16);
+		lblPrice.setBounds(291, 165, 61, 16);
 		contentPane.add(lblPrice);
 		
-		JButton btnAddStock = new JButton("Add Stock");
+		JButton btnAddStock = new JButton("Add Stock (type)");
 		btnAddStock.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//take the info from the text fields and use it to add a Stock to the db
@@ -88,10 +86,10 @@ public class EditStocksGUI extends JFrame {
 				listStocks.setListData(displayableStocks);
 			}
 		});
-		btnAddStock.setBounds(180, 231, 117, 29);
+		btnAddStock.setBounds(159, 226, 132, 29);
 		contentPane.add(btnAddStock);
 		
-		JButton btnDeleteStock = new JButton("Delete Stock");
+		JButton btnDeleteStock = new JButton("Delete Stock (Select)");
 		btnDeleteStock.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//remove the selected stock in the JList from the db
@@ -105,10 +103,10 @@ public class EditStocksGUI extends JFrame {
 				listStocks.setListData(displayableStocks);
 			}
 		});
-		btnDeleteStock.setBounds(30, 226, 117, 29);
+		btnDeleteStock.setBounds(0, 226, 162, 29);
 		contentPane.add(btnDeleteStock);
 		
-		JButton btnChangePrice = new JButton("Change Price");
+		JButton btnChangePrice = new JButton("Change Price (type)");
 		btnChangePrice.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//take the selected stock from the JList and change the price associated with it in the db
@@ -122,8 +120,19 @@ public class EditStocksGUI extends JFrame {
 				listStocks.setListData(displayableStocks);
 			}
 		});
-		btnChangePrice.setBounds(324, 231, 117, 29);
+		btnChangePrice.setBounds(285, 226, 153, 29);
 		contentPane.add(btnChangePrice);
+		
+		JButton btn = new JButton("Close");
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				AdminGUI adminGUI = new AdminGUI();
+				adminGUI.setVisible(true);
+			}
+		});
+		btn.setBounds(343, 154, 107, 27);
+		contentPane.add(btn);
 	}
 
 }
